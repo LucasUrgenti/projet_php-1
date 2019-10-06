6c4084173367458bb83a2d4d1ef7873bd5b3197e
Laurent Vouriot <49270550+laurent-vouriot@users.noreply.github.com>
2019-10-06 22:27:54 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,33 +3,47 @@
     <head lang="fr">
       <meta charset="UTF-8">
       <link rel="stylesheet" href="style/style.css">
+      <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Schoolbell&display=swap" rel="stylesheet">
+
       <title> FreeNote </title>
     </head>
     <body>
       <header class="header">
-        <h1> FreeNote </h1>
-        <div> Login </div>
+        <h1 class="h1_freeNote"> <a href="index.php"> FreeNote </a> </h1>
+        <ul id="ul_topBar">
+          <li id="li_topBar"> <a href="login.php"> Login </a> </li>
+        </ul>
       </header>
       <div class="div_central">
-        <section class="section">   
-          <p>
+        <aside class="aside">
+          <article>
+            <p> discussion 1
+              statut : fermée </p>
+            <hr>
+            </article>
+            <article>
+              <p> discussion 2
+                statut : ouvert </p>
+              <hr>
+              </article>
+              <article>
+                <p> discussion 3
+                  statut : ouvert </p>
+                <hr>
+                </article>
+            </aside>
+        <section class="section">
+          <article>
+            <p>
             Bienvenu sur FreeNote un réseau social d'un nouveau genre !
             Completez les phrases des autres de manière  super drôle, super chouettos !
           </p>
-        </section>
-        <aside>
-          <article>
-            <p> discussion 1
-                statut : fermée </p>
-          </article>
-          <article>
-            <p> discussion
-                statut : ouvert </p>
         </article>
-        </aside>
+        </section>
       </div><!--div central -->
-      <footer>
+      <footer class="footer">
           <p> footer test  </p>
       </footer>
     </body>
-  </html>
+  </html
