eaf03701812f62ce49fa9ac149c2be03b5883f38
Laurent Vouriot <49270550+laurent-vouriot@users.noreply.github.com>
2019-10-07 23:04:21 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,14 +5,36 @@
       <link rel="stylesheet" href="style/style.css">
       <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
       <link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Schoolbell&display=swap" rel="stylesheet">
-
       <title> FreeNote </title>
     </head>
     <body>
+
+
+      <script type="text/javascript">
+          function testSwipe()   {
+          var div = document.getElementById('test'),
+          display = getComputedStyle(div, null).display;
+
+          if(display == "block")  {
+              document.getElementById("test").style.display = "none";
+          }
+          else {
+              document.getElementById("test").style.display = "block";
+            }
+          }
+      </script>
+
+
       <header class="header">
         <h1 class="h1_freeNote"> <a href="index.php"> FreeNote </a> </h1>
         <ul id="ul_topBar">
-          <li id="li_topBar"> <a href="login.php"> Login </a> </li>
+          <li id="li_topBar" onclick="testSwipe()"> Login </a> </li>
+          <div id="test">
+            <form id = "form_login">
+              <li id="li_topBar"> test </li>
+            </form>
+          </div>
+          <li id="li_topBar"> <a href="inscription.php"> inscription </a> </li>
         </ul>
       </header>
       <div class="div_central">
@@ -43,7 +65,10 @@
         </section>
       </div><!--div central -->
       <footer class="footer">
-          <p> footer test  </p>
+        <a href="https://github.com/leo-dalloz" > Léo Dalloz </a>
+        <a href="https://github.com/jeremy-pouzargues" > Jérémy Pouzargues  </a>
+        <a href="https://github.com/laurent-vouriot" > Laurent Vouriot </a>
+        <a href="https://github.com/audreywagner" > Audrey Wagner </a>
       </footer>
     </body>
   </html
